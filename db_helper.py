9be2468e128568daf2f321abfba3a6a74e47a70a
dev-jaemin <jay.kimjaemin@gmail.com>
2023-04-28 22:48:31 +0900

    add: db_helper for psql

---
+++ db_helper.py
@@ -0,0 +1,66 @@
+from psycopg2 import pool
+from psycopg2 import extras
+
+
+class ConnectionPool:
+    """
+    PostgreSQL Connection Pool 클래스
+    """
+    __pool = None
+
+    def __init__(self, minconn: int, maxconn: int, **kwargs):
+        """
+        Connection Pool 객체 초기화
+        :param minconn: 최소 연결 수
+        :param maxconn: 최대 연결 수
+        :param kwargs: PostgreSQL 연결 설정
+        """
+        self.minconn = minconn
+        self.maxconn = maxconn
+        self.kwargs = kwargs
+        self._create_pool()
+
+    def _create_pool(self):
+        """
+        Connection Pool 생성
+        """
+        if not self.__pool:
+            try:
+                self.__pool = pool.SimpleConnectionPool(
+                    self.minconn, self.maxconn, **self.kwargs
+                )
+            except Exception as e:
+                print("Error: ", e)
+
+    def get_connection(self):
+        if self.__pool == None:
+            self._create_pool()
+
+        return self.__pool.getconn()
+
+    def put_connection(self, connection):
+        self.__pool.putconn(connection)
+
+    def close_all_connections(self):
+        self.__pool.closeall()
+
+    def insert_data(self, table_name: str, columns: list, data: list):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        columns_str = ','.join(columns)
+
+        # values = ','.join(['%s'] * len(columns))
+
+        insert_query = f"INSERT INTO {table_name} ({columns_str}) VALUES %s"
+
+        # 데이터 INSERT
+        try:
+            extras.execute_values(cursor, insert_query, data)
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            print("Error: ", e)
+        finally:
+            cursor.close()
+            self.put_connection(conn)
