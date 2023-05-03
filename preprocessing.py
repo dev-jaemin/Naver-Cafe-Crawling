12cb84fb55923e04923787f81e6639bacd1f0731
dev-jaemin <jay.kimjaemin@gmail.com>
2023-05-03 19:47:02 +0900

    ftr: Korean similar nickname

--- preprocessing.py
+++ preprocessing.py
@@ -2,28 +2,7 @@ class Preprocessing:
     def label_nickname(self, nickname):
         # E - I | N - S | F - T | J - P (alphabet order)
         MBTI_LIST = ["enfj", "enfp", "entj", "entp", "esfj", "esfp", "estj", "estp", "infj", "infp", "intj", "intp", "isfj", "isfp", "istj", "istp"]
-        MBTI_KEYWORDS = {
-            "엔프제": "enfj",
-            "엔프피": "enfp",
-            "엥쁘삐": "enfp",
-            "엥뿌삐": "enfp",
-            "엥뿌피": "enfp",
-            "엥쁘피": "enfp",
-            "엔티제": "entj", 
-            "엔팁": "entp", 
-            "엣프제": "esfj", 
-            "엣프피": "esfp", 
-            "엣티제": "estj", 
-            "엣팁": "estp", 
-            "인프제": "infj", 
-            "인프피": "infp", 
-            "인티제": "intj", 
-            "인팁": "intp", 
-            "잇프제": "isfj", 
-            "잇프피": "isfp", 
-            "잇티제": "istj", 
-            "잇팁": "istp"
-        }
+        MBTI_KEYWORDS = self._get_mbti_keywords()
 
         lower_nickname = nickname.lower()
 
@@ -39,6 +18,30 @@ class Preprocessing:
         
         # if there is no keywords, return False
         return False
+
+    def _get_mbti_keywords(self):
+        prefixes = {
+            'en': ['엔', '엥'],
+            'es': ['엣'],
+            'in': ['인', '잉'],
+            'is': ['잇']
+        }
+        suffixes = {
+            'fp': ['프피', '뿌피', '뿌삐', '픞', '픕'],
+            'fj': ['프제', '픚'],
+            'tp': ['팁', '팊', '티피', '티삐'],
+            'tj': ['티제', '팆']
+        }
+
+        korean_mbti_dic = {}
+
+        for p_eng, p_kors in prefixes.items():
+            for s_eng, s_kors in suffixes.items():
+                for p in p_kors:
+                    for s in s_kors:
+                        korean_mbti_dic[p + s] = p_eng + s_eng
+
+        return korean_mbti_dic
 
     def last_n_sentences(self, sentences, n):
         last_n_tuple = sentences[(-1) * n:]
