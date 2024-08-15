ee519c48919be39925b4b955cfe46188a5d65955
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-08-15 11:49:43 +0900

    fix: 안쓰는 테이블 스키마 정리

--- create.sql
+++ create.sql
@@ -1,22 +1,6 @@
 -- Table: public.qna
 
--- DROP TABLE IF EXISTS public.qna;
-
-CREATE TABLE IF NOT EXISTS public.qna
-(
-    id serial NOT NULL,
-    article_id integer NOT NULL,
-    menu_id integer NOT NULL,
-    question character varying(500) COLLATE pg_catalog."default" NOT NULL,
-    answer character varying(500) COLLATE pg_catalog."default" NOT NULL,
-    q_mbti character(4) COLLATE pg_catalog."default",
-    a_mbti character(4) COLLATE pg_catalog."default",
-    CONSTRAINT qna_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.qna
-    OWNER to postgres;
+-- DROP TABLE IF EXISTS public.multiple_qna;
 
 CREATE TABLE IF NOT EXISTS public.multiple_qna
 (
