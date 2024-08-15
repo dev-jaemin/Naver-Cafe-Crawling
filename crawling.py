9777e78ef9f9423595d90b2b1324c04ef34533e4
Jaemin Kim <jay.kimjaemin@gmail.com>
2024-08-15 11:50:24 +0900

    feat: board_id와 page_count는 인풋으로 받기

--- crawling.py
+++ crawling.py
@@ -5,6 +5,9 @@ from navercafe import NaverCafe
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
+board_ids = [int(n) for n in input("크롤링하고 싶은 게시판의 id를 띄어쓰기로 구분해서 입력해주세요(ex. 11 12 13) : ").split()]
+page_count = int(input("크롤링 진행할 페이지 수를 지정해주세요. 시간이 오래 걸리니 우선 작은 숫자로 시작해보는건 어떨까요? : "))
+
 # 1. setup
 cafe_name = os.environ['CAFE_NAME']
 club_id = os.environ['CAFE_ID']
@@ -14,10 +17,9 @@ cafe = NaverCafe(cafe_name, club_id)
 # This is semi-automatic (needs manual authentication)
 cafe.enter_id_pw(os.environ['NAVER_ID'], os.environ['NAVER_PW'])
 
-board_ids = [11]
 for board_id in board_ids:
     # 3. get article board ids
-    article_ids = cafe.get_article_ids(board_id, 500)
+    article_ids = cafe.get_article_ids(board_id, page_count)
 
     # 4. get articles, QNA
     cafe.get_articles(board_id, article_ids)
